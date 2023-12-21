6353f2cf1cf9da80b89e8a85d8c1e3d4fe915f6b
SunDoge <384813529@qq.com>
2023-12-21 15:19:06 +0800

    save

--- rust-webui/src/webui.rs
+++ rust-webui/src/webui.rs
@@ -3,12 +3,31 @@ use std::{
     borrow::Borrow,
     collections::HashMap,
     ffi::CString,
+    path::Path,
     sync::{Mutex, RwLock},
 };
 
 use webui_sys as ffi;
 
 static CALLBACKS: OnceCell<Mutex<HashMap<usize, Box<dyn Fn() + Send + 'static>>>> = OnceCell::new();
+
+#[derive(Debug)]
+#[repr(usize)]
+pub enum Browser {
+    NoBrowser = 0,
+    AnyBrowser = 1,
+    Chrome,
+    Firefox,
+    Edge,
+    Safari,
+    Chromium,
+    Opera,
+    Brave,
+    Vivaldi,
+    Epic,
+    Yandex,
+    ChromiumBased,
+}
 
 pub struct Window(usize);
 
@@ -30,8 +49,30 @@ impl Window {
         unsafe { ffi::webui_show(self.0, cstring.as_ptr()) }
     }
 
+    pub fn show_browser(&self, content: &str, browser: Browser) -> bool {
+        let cstring = CString::new(content).unwrap();
+        unsafe { ffi::webui_show_browser(self.0, cstring.as_ptr(), browser as usize) }
+    }
+
     pub fn is_shown(&self) -> bool {
         unsafe { ffi::webui_is_shown(self.0) }
+    }
+
+    pub fn set_size(&mut self, width: u32, height: u32) {
+        unsafe {
+            ffi::webui_set_size(self.0, width, height);
+        }
+    }
+
+    pub fn set_position(&mut self, x: u32, y: u32) {
+        unsafe {
+            ffi::webui_set_position(self.0, x, y);
+        }
+    }
+
+    pub fn set_root_folder(&mut self, path: impl AsRef<Path>) -> bool {
+        let cstring = CString::new(path.as_ref().as_os_str().to_str().unwrap()).unwrap();
+        unsafe { ffi::webui_set_root_folder(self.0, cstring.as_ptr()) }
     }
 
     pub fn set_icon(&self, icon: &str, icon_type: &str) {
@@ -67,5 +108,17 @@ unsafe extern "C" fn event_handler(
 pub fn wait() {
     unsafe {
         ffi::webui_wait();
+    }
+}
+
+pub fn clean() {
+    unsafe {
+        ffi::webui_clean();
+    }
+}
+
+pub fn set_timeout(second: usize) {
+    unsafe {
+        ffi::webui_set_timeout(second);
     }
 }
